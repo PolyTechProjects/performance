92139c489e8536222a47e529403bda3536296f29
Yaroslav Shutov <yarshutov@yandex.ru>
2024-03-26 00:46:05 +0300

    fix: added declaring exchange

--- src/main/kotlin/ru/aps/performance/configs/RabbitMqConfig.kt
+++ src/main/kotlin/ru/aps/performance/configs/RabbitMqConfig.kt
@@ -2,6 +2,7 @@ package ru.aps.performance.configs
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Bean
+import org.springframework.amqp.rabbit.core.RabbitAdmin
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
@@ -25,6 +26,13 @@ class RabbitMqConfig() {
     fun rabbitTemplate(connectionFactory: ConnectionFactory): RabbitTemplate {
         val rabbitTemplate = RabbitTemplate(connectionFactory)
         return rabbitTemplate
+    }
+
+    @Bean
+    fun rabbitAdmin(connectionFactory: ConnectionFactory): RabbitAdmin {
+        val rabbitAdmin = RabbitAdmin(connectionFactory)
+        rabbitAdmin.declareExchange(exchange())
+        return rabbitAdmin;
     }
 
     @Bean
